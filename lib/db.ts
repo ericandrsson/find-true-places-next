7abdc510d7d49a6497d8de69d66b0308bb103349
Eric Andersson <eric.andersson@ilpvfx.com>
2024-09-11 14:34:33 +0200

    automatic

--- lib/db.ts
+++ lib/db.ts
@@ -6,6 +6,4 @@ export interface Category {
   icon: string;
 }
 
-export const pb = new PocketBase(
-  process.env.NEXT_PUBLIC_POCKETBASE_URL || "http://127.0.0.1:8090"
-);
+export const pb = new PocketBase(process.env.NEXT_PUBLIC_POCKETBASE_URL || "");
